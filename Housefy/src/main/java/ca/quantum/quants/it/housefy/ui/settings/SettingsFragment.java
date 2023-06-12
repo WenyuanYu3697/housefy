c68aa645c7c6981e230d99c47b3d542f045b603e
kyrylolvov <kyrylolvov@icloud.com>
2023-06-11 22:09:48 -0400

    CHANGED: - Added orientation change switch - Added notifications switch - Added temperature and air quality threshold inputs

--- Housefy/src/main/java/ca/quantum/quants/it/housefy/ui/settings/SettingsFragment.java
+++ Housefy/src/main/java/ca/quantum/quants/it/housefy/ui/settings/SettingsFragment.java
@@ -7,10 +7,13 @@ package ca.quantum.quants.it.housefy.ui.settings;
  * @course Software Project - CENG-322-0NA
  */
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,11 +21,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import ca.quantum.quants.it.housefy.R;
 import ca.quantum.quants.it.housefy.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding binding;
+    private Switch switchOrientation;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,6 +36,19 @@ public class SettingsFragment extends Fragment {
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        switchOrientation = root.findViewById(R.id.orientation_switch);
+
+        switchOrientation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                } else {
+                    getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                }
+            }
+        });
 
         settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<Integer>() {
             @Override
