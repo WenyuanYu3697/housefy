a8b233381835253d5745397d7b5e4eb2776c5ebb
kyrylolvov <kyrylolvov@icloud.com>
2023-07-10 01:53:27 -0400

    CHANGED: - Added EnergyUsageCard component

---
+++ housefy/src/main/java/ca/quantum/quants/it/housefy/components/home/EnergyUsage.kt
@@ -0,0 +1,105 @@
+package ca.quantum.quants.it.housefy.components.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EnergyUsageCard() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Energy Usage",
+                style = MaterialTheme.typography.titleMedium
+                    .copy(fontWeight = FontWeight.Medium)
+                    .copy(color = Color(0xFF353336)),
+            )
+            Text(
+                text = "See All",
+                style = MaterialTheme.typography.bodySmall
+                    .copy(fontWeight = FontWeight.Medium)
+                    .copy(color = Color(0xFF353336)),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp, vertical = 8.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Jul 10, 2023",
+                    style = MaterialTheme.typography.bodySmall
+                        .copy(fontWeight = FontWeight.Medium)
+                        .copy(color = Color(0xFF353336)),
+                )
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Today",
+                    style = MaterialTheme.typography.bodySmall
+                        .copy(fontWeight = FontWeight.SemiBold)
+                        .copy(color = Color(0xFFA3A3A5)),
+                )
+                Text(
+                    text = "This month",
+                    style = MaterialTheme.typography.bodySmall
+                        .copy(fontWeight = FontWeight.SemiBold)
+                        .copy(color = Color(0xFFA3A3A5)),
+                )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "55.0 kWh",
+                    style = MaterialTheme.typography.bodyMedium
+                        .copy(fontWeight = FontWeight.Medium)
+                        .copy(color = Color(0xFF353336)),
+                )
+                Text(
+                    text = "234.0 kWh",
+                    style = MaterialTheme.typography.bodyMedium
+                        .copy(fontWeight = FontWeight.Medium)
+                        .copy(color = Color(0xFF353336)),
+                )
+            }
+        }
+    }
+}
--- housefy/src/main/java/ca/quantum/quants/it/housefy/pages/HomePage.kt
+++ housefy/src/main/java/ca/quantum/quants/it/housefy/pages/HomePage.kt
@@ -2,22 +2,37 @@ package ca.quantum.quants.it.housefy.pages
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import ca.quantum.quants.it.housefy.components.home.EnergyUsageCard
 import ca.quantum.quants.it.housefy.components.home.TemperatureCard
 
 @Composable
 fun HomePage() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color(0xFFF0F2F1))
             .padding(16.dp),
     ) {
-        TemperatureCard()
+        Row(modifier = Modifier.fillMaxWidth()) {
+            TemperatureCard()
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            EnergyUsageCard()
+        }
+
     }
 }
