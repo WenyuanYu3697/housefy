d02f687154a9855b8695ac545aac1cfd3d1f3ab9
artdev1743 <artemtsurkan002@gmail.com>
2023-07-12 22:52:36 -0400

    ADDED: - Added the functionality to set threshold and reflect if the threshold is exceeded by changing the color of a chart column to red

--- housefy/src/main/java/ca/quantum/quants/it/housefy/pages/EnergyConsumptionPage.kt
+++ housefy/src/main/java/ca/quantum/quants/it/housefy/pages/EnergyConsumptionPage.kt
@@ -1,29 +1,19 @@
 package ca.quantum.quants.it.housefy.pages
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-//import androidx.compose.foundation.layout.ColumnScopeInstance.align
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-//import androidx.compose.material.icons.filled
-import androidx.compose.material.icons.filled.MoreVert
-//import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.Surface
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -31,17 +21,22 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import ca.quantum.quants.it.housefy.ui.theme.HousefyTheme
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.ui.Alignment.Companion.CenterVertically
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.zIndex
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EnergyConsumptionPage() {
-    Box(modifier = Modifier.fillMaxSize().background(color = Color(0xFFF0F2F1)), contentAlignment = Alignment.Center) {
-        //Text(text = "Energy Consumption Page Content")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFF0F2F1)),
+        contentAlignment = Alignment.Center) {
+        var threshold by remember { mutableStateOf(0.7f) } // Initializing threshold to 0
+        // Your Chart
         Chart(
             data = listOf(
                 Pair(0.9f, 1),
@@ -51,14 +46,51 @@ fun EnergyConsumptionPage() {
                 Pair(0.8f, 5),
                 Pair(0.7f, 6),
                 Pair(0.7f, 7),
-            ), max_value = 50)
+            ), max_value = 50, threshold = threshold
+        )
+        //Smaller box for setting the threshold
+        Box(
+            modifier = Modifier
+                //.padding(top = 40.dp)
+                .padding(30.dp, 70.dp, 30.dp, 500.dp)
+                .absoluteOffset(0.dp, 460.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(5))
+                .background(color = Color(0xFFFFFFFF)),
+            contentAlignment = Alignment.Center
+        ) {
+            Column() {
+                Text(
+                    text = "Set Threshold",
+                    color = Color(0xFF7468E4),
+                    textAlign = TextAlign.Start,
+                    //modifier = Modifier
+                      //  .fillMaxWidth()
+                       // .padding(25.dp, 20.dp)
+                    )
+            }
+
+            Column() {
+                TextField(
+                    value = threshold.toString(),
+                    onValueChange = { threshold = it.toFloatOrNull() ?: threshold },
+                    label = { Text(text = "Set Threshold") },
+                    modifier = Modifier
+                        .background(color = Color(0xFFFFFFFF))
+                        //.padding(top = 10.dp, start = 270.dp, end = 5.dp)
+                )
+            }
+        }
+
     }
 }
 
 @Composable
 fun Chart(
     data: List<Pair<Float, Int>>,
-    max_value: Int
+    max_value: Int,
+    threshold: Float // Adding threshold as a parameter
 ) {
     val context = LocalContext.current
 
@@ -66,14 +98,10 @@ fun Chart(
         modifier = Modifier
             .padding(30.dp, 60.dp, 30.dp, 180.dp)
             .clip(RoundedCornerShape(5))
-            //.background(Color(0xFF333340))
-            //.background(Color(0xFFA3A3A5))
-              .background(Color(0xFFFFFFFF))
-        //.background(Color.Red)
+            .background(Color(0xFFFFFFFF))
     ) {
         Column() {
             Text(
-
                 text = "Usage, KWh",
                 fontWeight = FontWeight.Bold,
                 color = Color(0xFF7468E4),
@@ -84,6 +112,7 @@ fun Chart(
                     .padding(25.dp, 20.dp)
             )
         }
+
         Column() {
             Row(
                 modifier = Modifier
@@ -107,21 +136,16 @@ fun Chart(
                         imageVector = Icons.Filled.MoreVert,
                         contentDescription = "Options",
                         tint = Color.White,
-                        //modifier = Modifier
-                            //.align(Alignment.Top)
-
                     )
                 }
             }
-
         }
-//GRAPH BOX IMPLEMENTED WITH COLUMN
+
         Column(
             modifier = Modifier
                 .absoluteOffset(0.dp, 120.dp)
                 .padding(20.dp, 5.dp, 20.dp, 225.dp)
                 .fillMaxSize()
-            //.background(Color.Magenta)
         ) {
             Row(
                 modifier = Modifier
@@ -136,7 +160,7 @@ fun Chart(
 
                 // Graph columns
                 data.forEach {
-                    Bar(height = it.first, onClick = {
+                    Bar(height = it.first, threshold = threshold, onClick = {
                         Toast.makeText(context, it.first.toString(), Toast.LENGTH_SHORT).show()
                     })
                 }
@@ -151,31 +175,21 @@ fun Chart(
 
             XAxis(data)
         }
-// GRAPH BOX-COLUMN END
 
-//TOTAL FOR TIME PERIOD START
         Box(
-            //verticalArrangement = Arrangement.Top,
             modifier = Modifier
                 .fillMaxWidth()
                 .absoluteOffset(28.dp, 370.dp)
-            //.padding(56.dp, 300.dp)
         ){
             Text(
                 text = "Total: 254 KWh",
                 fontWeight = FontWeight.Bold,
-                //color = Color(0xFFFFFFFF),
                 color = Color(0xFF7C7A7A),
-                //color = Color.Black,
                 fontSize = 30.sp,
                 textAlign = TextAlign.Start,
-
-                )
+            )
         }
-// TOTAL FOR TIME PERIOD END
-
     }
-
 }
 
 @Composable
@@ -225,15 +239,14 @@ fun YAxis(maxValue: Int) {
 }
 
 @Composable
-fun Bar(height: Float, onClick: () -> Unit) {
+fun Bar(height: Float, threshold: Float, onClick: () -> Unit) {
     Box(
         modifier = Modifier
-            //makes columns closer to each other
             .padding(start = 15.dp)
             .clip(RoundedCornerShape(10.dp))
             .width(15.dp)
             .fillMaxHeight(height)
-            .background(Color(0xFF7468E4))
+            .background(if (height > threshold) Color.Red else Color(0xFF7468E4)) // Choose color based on threshold
             .clickable { onClick() }
     )
 }
@@ -242,10 +255,8 @@ fun Bar(height: Float, onClick: () -> Unit) {
 fun XAxis(data: List<Pair<Float, Int>>) {
     Row(
         modifier = Modifier
-            //remoteness of 1st column from y-axis
             .padding(start = 62.dp)
             .fillMaxWidth(),
-        //distance between x axis values
         horizontalArrangement = Arrangement.spacedBy(10.dp)
     ) {
         data.forEach {
@@ -258,4 +269,3 @@ fun XAxis(data: List<Pair<Float, Int>>) {
         }
     }
 }
-
