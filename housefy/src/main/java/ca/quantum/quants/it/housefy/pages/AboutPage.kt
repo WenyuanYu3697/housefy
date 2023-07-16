6d81c4f50b423340bc1b2e8d9b269aec52894d69
kyrylolvov <kyrylolvov@icloud.com>
2023-07-16 03:27:23 -0400

    CHANGED: - Updated About and Guide pages with proper background color and padding

--- housefy/src/main/java/ca/quantum/quants/it/housefy/pages/AboutPage.kt
+++ housefy/src/main/java/ca/quantum/quants/it/housefy/pages/AboutPage.kt
@@ -1,6 +1,7 @@
 package ca.quantum.quants.it.housefy.pages
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
@@ -12,6 +13,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -20,33 +22,27 @@ import androidx.compose.ui.unit.sp
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutPage() {
-    MaterialTheme {
-        Scaffold(
-            //topBar = {
-            //    TopAppBar(title = { Text("Guide") })
-            //}
-        ) {
-            LazyColumn(
-                modifier = Modifier
-                    .padding(top=25.dp, start = 20.dp, end= 20.dp),
-                // .padding(16.dp),
-
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                item {
-                    Text(text = "About Housefy\n",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold)
-                    Text(text = "Housefy is an innovative smart home Internet of Things (IoT) application that seamlessly integrates technology into every corner of your home, elevating your everyday living experience.\n" +
-                            "\n" +
-                            "We are dedicated to providing an unparalleled smart home experience that places simplicity, control, and convenience right at your fingertips. With the use of cutting-edge technology, Housefy effortlessly interconnects all your home devices, giving you the power to control and monitor your home, no matter where you are.\n" +
-                            "\n" +
-                            "Our app is designed with a user-friendly interface that makes it easy to manage a variety of home devices. From security systems and lighting controls to thermostats and home appliances, Housefy enables you to run your home in a smarter, more energy-efficient way. Whether you are at home or on the go, Housefy keeps you connected to what's important the most.\n" +
-                            "\n" +
-                            "Housefy is more than just an app - it's a lifestyle. It offers the convenience of smart automation, the comfort of energy efficiency and the peace of mind of remote control. It's your home, but smarter. Welcome to Housefy – embrace the future of home automation.\n")
-                }
-
-            }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFF0F2F1))
+            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+    ) {
+        item {
+            Text(
+                text = "About Housefy\n",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Housefy is an innovative smart home Internet of Things (IoT) application that seamlessly integrates technology into every corner of your home, elevating your everyday living experience.\n" +
+                        "\n" +
+                        "We are dedicated to providing an unparalleled smart home experience that places simplicity, control, and convenience right at your fingertips. With the use of cutting-edge technology, Housefy effortlessly interconnects all your home devices, giving you the power to control and monitor your home, no matter where you are.\n" +
+                        "\n" +
+                        "Our app is designed with a user-friendly interface that makes it easy to manage a variety of home devices. From security systems and lighting controls to thermostats and home appliances, Housefy enables you to run your home in a smarter, more energy-efficient way. Whether you are at home or on the go, Housefy keeps you connected to what's important the most.\n" +
+                        "\n" +
+                        "Housefy is more than just an app - it's a lifestyle. It offers the convenience of smart automation, the comfort of energy efficiency and the peace of mind of remote control. It's your home, but smarter. Welcome to Housefy – embrace the future of home automation.\n"
+            )
         }
     }
 }
