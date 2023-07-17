841dcf4b79b40c335b0dfdc4e0c6aaf77e35fc28
kyrylolvov <kyrylolvov@icloud.com>
2023-07-17 11:44:38 -0400

    CHANGED: - Added SettingsItem component - Implemented Lock Portrait functionality

---
+++ housefy/src/main/java/ca/quantum/quants/it/housefy/components/settings/SettingsItem.kt
@@ -0,0 +1,37 @@
+package ca.quantum.quants.it.housefy.components.settings
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SettingsRow(
+    title: String,
+    control: @Composable () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.bodyMedium
+                .copy(fontWeight = FontWeight.Medium)
+                .copy(color = Color(0xFF353336)),
+        )
+
+        control()
+    }
+}
--- housefy/src/main/java/ca/quantum/quants/it/housefy/pages/SettingsPage.kt
+++ housefy/src/main/java/ca/quantum/quants/it/housefy/pages/SettingsPage.kt
@@ -1,12 +1,62 @@
 package ca.quantum.quants.it.housefy.pages
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import ca.quantum.quants.it.housefy.components.settings.SettingsRow
 
 @Composable
 fun SettingsPage() {
-    Box() {
-        Text(text = "Settings Page")
+    val context = LocalContext.current
+    val activity = context as? Activity
+
+    val lockPortrait = remember { mutableStateOf(false) }
+    val enableNotifications = remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFF0F2F1))
+            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+    ) {
+
+        Column() {
+            SettingsRow(
+                title = "Lock Portrait",
+                control = {
+                    Switch(
+                        checked = lockPortrait.value,
+                        onCheckedChange = { newValue ->
+                            lockPortrait.value = newValue
+                            activity?.requestedOrientation = if (newValue) {
+                                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                            } else {
+                                ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+                            }
+                        }
+                    )
+                }
+            )
+
+            SettingsRow(
+                title = "Enable Notifications",
+                control = {
+                    Switch(
+                        checked = enableNotifications.value,
+                        onCheckedChange = { enableNotifications.value = it }
+                    )
+                }
+            )
+        }
     }
 }
