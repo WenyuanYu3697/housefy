b88b67c356d66e58eb4f8f14030dbd29239187bd
WenyuanYu3697 <bruce2000525@outlook.com>
2023-07-16 22:51:10 -0400

    CHANGED: - Modified AirQualityPage to include the graph and categories

--- housefy/src/main/java/ca/quantum/quants/it/housefy/pages/AirQualityPage.kt
+++ housefy/src/main/java/ca/quantum/quants/it/housefy/pages/AirQualityPage.kt
@@ -1,235 +1,76 @@
 package ca.quantum.quants.it.housefy.pages
 
 import androidx.compose.material3.*
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardOptions
-//import androidx.compose.material.MaterialTheme
-//import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import ca.quantum.quants.it.housefy.components.air_quality.AQICategory
+import ca.quantum.quants.it.housefy.components.air_quality.AQICategoryCard
+import ca.quantum.quants.it.housefy.components.air_quality.AirQualityGraph
+import ca.quantum.quants.it.housefy.components.air_quality.getAQIColor
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AirQualityPage() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        //Text(text = "Air Quality Page Content")
-        
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFF0F2F1)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
             var value by remember {
-                mutableStateOf(0)
+                mutableStateOf(20)
             }
-            CustomComponent(
-                indicatorValue = value
-            )
 
-            TextField(value = value.toString(), onValueChange = {
-                value = if (it.isNotEmpty()){
-                    it.toInt()
-                }else
-                {
-                    0
-                }
-            },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number
-                )
+            AirQualityGraph(
+                indicatorValue = value,
+                foregroundIndicatorColor = getAQIColor(value)
             )
+        }
 
+        val aqiCategories = listOf(
+            AQICategory(
+                "Excellent",
+                "Air quality is satisfactory with minimal or no risk of air pollution.",
+                Color(0xFF8CD456),
+            ),
+            AQICategory(
+                "Good",
+                "Air quality is acceptable. Some pollutants may have a moderate impact on a small number of sensitive individuals.",
+                Color(0xFFFFE24C),
+            ),
+            AQICategory(
+                "Moderate",
+                "Some sensitive individuals may experience health effects. The general public is unlikely to be affected.",
+                Color(0xFFFFA500),
+            ),
+            AQICategory(
+                "Poor",
+                "Health effects may occur in the general public, and sensitive groups may experience more severe health issues.",
+                Color(0xFFFF0000),
+            ),
+        )
+
+        item { Spacer(modifier = Modifier.height(24.dp)) }
+
+        items(aqiCategories) { category ->
+            Box(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+                AQICategoryCard(category)
+            }
         }
 
     }
-}
-
-@Composable
-fun CustomComponent(
-    canvasSize: Dp = 300.dp,
-    indicatorValue: Int = 0,
-    maxIndicatorValue: Int = 100,
-    backgroundIndicatorColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
-    backgroundIndicatorStrokeWidth: Float = 100f,
-    foregroundIndicatorColor: Color = MaterialTheme.colorScheme.primary,
-    foregroundIndicatorStrokeWidth: Float = 100f,
-//    indicatorStrokeCap: StrokeCap = StrokeCap.Round,
-    bigTextFontSize: TextUnit = MaterialTheme.typography.titleLarge.fontSize,
-    bigTextColor: Color = MaterialTheme.colorScheme.onSurface,
-    bigTextSuffix: String = "AQI",
-    smallText: String = "Air Quality Index",
-    smallTextFontSize: TextUnit = MaterialTheme.typography.titleLarge.fontSize,
-    smallTextColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f)
-) {
-    var allowedIndicatorValue by remember {
-        mutableStateOf(maxIndicatorValue)
-    }
-    allowedIndicatorValue = if (indicatorValue <= maxIndicatorValue) {
-        indicatorValue
-    } else {
-        maxIndicatorValue
-    }
-
-    var animatedIndicatorValue by remember { mutableStateOf(0f) }
-    LaunchedEffect(key1 = allowedIndicatorValue) {
-        animatedIndicatorValue = allowedIndicatorValue.toFloat()
-    }
-
-    val percentage =
-        (animatedIndicatorValue / maxIndicatorValue) * 100
-
-    val sweepAngle by animateFloatAsState(
-        targetValue = (2.4 * percentage).toFloat(),
-        animationSpec = tween(1000)
-    )
-
-    val receivedValue by animateIntAsState(
-        targetValue = allowedIndicatorValue,
-        animationSpec = tween(1000)
-    )
-
-    val animatedBigTextColor by animateColorAsState(
-        targetValue = if (allowedIndicatorValue == 0)
-            MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f)
-        else
-            bigTextColor,
-        animationSpec = tween(1000)
-    )
-
-    Column(
-        modifier = Modifier
-            .size(canvasSize)
-            .drawBehind {
-                val componentSize = size / 1.25f
-                backgroundIndicator(
-                    componentSize = componentSize,
-                    indicatorColor = backgroundIndicatorColor,
-                    indicatorStrokeWidth = backgroundIndicatorStrokeWidth,
-//                    indicatorStokeCap = indicatorStrokeCap
-                )
-                foregroundIndicator(
-                    sweepAngle = sweepAngle,
-                    componentSize = componentSize,
-                    indicatorColor = foregroundIndicatorColor,
-                    indicatorStrokeWidth = foregroundIndicatorStrokeWidth,
-//                    indicatorStokeCap = indicatorStrokeCap
-                )
-            },
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        EmbeddedElements(
-            bigText = receivedValue,
-            bigTextFontSize = bigTextFontSize,
-            bigTextColor = animatedBigTextColor,
-            bigTextSuffix = bigTextSuffix,
-            smallText = smallText,
-            smallTextColor = smallTextColor,
-            smallTextFontSize = smallTextFontSize
-        )
-    }
-}
-
-fun DrawScope.backgroundIndicator(
-    componentSize: Size,
-    indicatorColor: Color,
-    indicatorStrokeWidth: Float,
-//    indicatorStokeCap: StrokeCap
-) {
-    drawArc(
-        size = componentSize,
-        color = indicatorColor,
-        startAngle = 150f,
-        sweepAngle = 240f,
-        useCenter = false,
-        style = Stroke(
-            width = indicatorStrokeWidth,
-            cap = StrokeCap.Round
-        ),
-        topLeft = Offset(
-            x = (size.width - componentSize.width) / 2f,
-            y = (size.height - componentSize.height) / 2f
-        )
-    )
-}
-
-fun DrawScope.foregroundIndicator(
-    sweepAngle: Float,
-    componentSize: Size,
-    indicatorColor: Color,
-    indicatorStrokeWidth: Float,
-//    indicatorStokeCap: StrokeCap
-) {
-    drawArc(
-        size = componentSize,
-        color = indicatorColor,
-        startAngle = 150f,
-        sweepAngle = sweepAngle,
-        useCenter = false,
-        style = Stroke(
-            width = indicatorStrokeWidth,
-            cap = StrokeCap.Round
-        ),
-        topLeft = Offset(
-            x = (size.width - componentSize.width) / 2f,
-            y = (size.height - componentSize.height) / 2f
-        )
-    )
-}
-
-@Composable
-fun EmbeddedElements(
-    bigText: Int,
-    bigTextFontSize: TextUnit,
-    bigTextColor: Color,
-    bigTextSuffix: String,
-    smallText: String,
-    smallTextColor: Color,
-    smallTextFontSize: TextUnit
-) {
-    Text(
-        text = smallText,
-        color = smallTextColor,
-        fontSize = smallTextFontSize,
-        textAlign = TextAlign.Center
-    )
-    Text(
-        text = "$bigText ${bigTextSuffix.take(3)}",
-        color = bigTextColor,
-        fontSize = bigTextFontSize,
-        textAlign = TextAlign.Center,
-        fontWeight = FontWeight.Bold
-    )
-}
-
-@Composable
-@Preview(showBackground = true)
-fun CustomComponentPreview() {
-    CustomComponent()
 }
