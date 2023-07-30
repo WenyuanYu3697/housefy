1614542d1da16b1bf68dda691895951dac05f9b2
bruc <bruce2000525@outlook.com>
2023-07-29 21:51:17 -0400

    AD 1.added validation and snackbar for the feedback page fullname userinput

--- housefy/src/main/java/ca/quantum/quants/it/housefy/pages/FeedbackPage.kt
+++ housefy/src/main/java/ca/quantum/quants/it/housefy/pages/FeedbackPage.kt
@@ -8,7 +8,6 @@ package ca.quantum.quants.it.housefy.pages
  */
 
 import android.os.Build
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
@@ -19,10 +18,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ca.quantum.quants.it.housefy.R
 import io.ktor.client.*
@@ -32,7 +29,6 @@ import io.ktor.http.*
 import io.ktor.http.content.TextContent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -51,7 +47,18 @@ fun FeedbackPage() {
     var dialogMessage by remember { mutableStateOf("") }
     var dialogVisible by remember { mutableStateOf(false) }
 
+    val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+
+    fun showSnackbarMessage(message: String) {
+        coroutineScope.launch {
+            snackbarHostState.showSnackbar(
+                message = message,
+                actionLabel = "Dismiss",
+                duration = SnackbarDuration.Short
+            )
+        }
+    }
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -119,16 +126,22 @@ fun FeedbackPage() {
                 contentAlignment = Alignment.Center
             ) {
                 Button(onClick = {
-                    coroutineScope.launch(Dispatchers.IO) {
-                        try {
-                            val user = User(fullName, email, phoneModel, phoneNumber)
-                            val feedback = Feedback(rating, comment, user)
-                            val result = postFeedback(feedback)
-                            dialogMessage = result.second
-                            dialogVisible = true
-                        } catch (e: Exception) {
-                            dialogMessage = "Error: ${e.localizedMessage}"
-                            dialogVisible = true
+                    when {
+                        fullName.length > 50 -> showSnackbarMessage("Name cannot exceed 50 characters.")
+                        fullName.any { it.isDigit() } -> showSnackbarMessage("Name should not contain numbers.")
+                        else -> {
+                            coroutineScope.launch(Dispatchers.IO) {
+                                try {
+                                    val user = User(fullName, email, phoneModel, phoneNumber)
+                                    val feedback = Feedback(rating, comment, user)
+                                    val result = postFeedback(feedback)
+                                    dialogMessage = result.second
+                                    dialogVisible = true
+                                } catch (e: Exception) {
+                                    dialogMessage = "Error: ${e.localizedMessage}"
+                                    dialogVisible = true
+                                }
+                            }
                         }
                     }
                 }) {
@@ -136,6 +149,16 @@ fun FeedbackPage() {
                 }
             }
         }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        SnackbarHost(
+            hostState = snackbarHostState,
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
     }
 
     if (dialogVisible) {
