f75fbb4c5a52a608e1b7b65f0367e9c1cac0e6ff
bruc <bruce2000525@outlook.com>
2023-08-04 22:41:14 -0400

    CD 1. moved the hardcoded into string.xml to avoid hardcoding

--- housefy/src/main/java/ca/quantum/quants/it/housefy/components/feedback/RatingBar.kt
+++ housefy/src/main/java/ca/quantum/quants/it/housefy/components/feedback/RatingBar.kt
@@ -1,5 +1,11 @@
 package ca.quantum.quants.it.housefy.components.feedback
 
+/*
+ * @author Artem Tsurkan, n01414146
+ * @author Wenyuan Yu, n01403697
+ * @author Kyrylo Lvov, n01414058
+ * @course Software Project - CENG-322-0NA
+ */
 import androidx.compose.material3.Icon
 import ca.quantum.quants.it.housefy.ui.theme.Purple
 import androidx.compose.foundation.clickable
